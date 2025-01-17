0a0bf312f3ae9999edbd80a3b31a0d216f6eeaec
Marvin <marvin.ogot@gmail.com>
2025-01-17 11:04:36 +0300

    arithmetic operators and working with Math and Number objects and their respective methods

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// Task 1
+let multiply;
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+multiply = num1 * num2;
+
+// Task 2
+let random;
+random = Math.random() * 10;
+
+// Task 3
+let mod;
+let num3 = 24;
+let num4 = 5;
+
+mod = num3 % num4;
+
+// Task 4
+let max;
+max = Math.max(1,5,9,15,20);
